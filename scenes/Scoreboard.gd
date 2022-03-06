a3fad55e2cfa7635735ffcdc862ff620b45fb85c
Lukas Wallenböck <lukas.wallenboeck@outlook.at>
2022-03-06 16:44:35 +0100

    Beautify scoreboard

--- scenes/Scoreboard.gd
+++ scenes/Scoreboard.gd
@@ -6,7 +6,10 @@ func _ready():
 	pass
 
 func set_scores(scores):
-	print(scores)
-	$Score.text = str(scores[0].name, ": ", scores[0].score)
-	$Score2.text = str(scores[1].name, ": ", scores[1].score)
-	$Score3.text = str(scores[2].name, ": ", scores[2].score)
+	$Name.text = scores[0].name + ":"
+	$Name2.text = scores[1].name + ":"
+	$Name3.text = scores[2].name + ":"
+	
+	$Score.text = str(scores[0].score)
+	$Score2.text = str(scores[1].score)
+	$Score3.text = str(scores[2].score)
